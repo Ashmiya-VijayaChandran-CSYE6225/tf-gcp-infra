04c94f3b0d74df51a6dd15cb56d6d860cf1891e9
AshmiyaV <ashmiyavs6767@gmail.com>
2024-02-15 15:34:44 -0500

    [Assignment 3] naming convention change

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 resource "google_compute_network" "vpc_network" {
   count                           = var.count_of_vpcs
-  name                            = var.count_of_vpcs == "1" ? var.vpc_network_name : "${var.vpc_network_name}-${count.index + 1}"
+  name                            = count.index < 1 ? var.vpc_network_name : "${var.vpc_network_name}-${count.index}"
   auto_create_subnetworks         = false
   routing_mode                    = var.routing_mode
   delete_default_routes_on_create = true
@@ -14,21 +14,21 @@ resource "google_compute_network" "vpc_network" {
 
 resource "google_compute_subnetwork" "app_network" {
   count         = var.count_of_vpcs
-  name          = var.count_of_vpcs == "1" ? var.webapp_subnetwork_name : "${var.webapp_subnetwork_name}-${count.index + 1}"
+  name          = count.index < 1 ? var.webapp_subnetwork_name : "${var.webapp_subnetwork_name}-${count.index}"
   ip_cidr_range = var.webapp_ip_cidr_range
   network       = google_compute_network.vpc_network[count.index].id
 }
 
 resource "google_compute_subnetwork" "db_network" {
   count         = var.count_of_vpcs
-  name          = var.count_of_vpcs == "1" ? var.db_subnetwork_name : "${var.db_subnetwork_name}-${count.index + 1}"
+  name          = count.index < 1 ? var.db_subnetwork_name : "${var.db_subnetwork_name}-${count.index}"
   ip_cidr_range = var.db_ip_cidr_range
   network       = google_compute_network.vpc_network[count.index].id
 }
 
 resource "google_compute_route" "compute_route" {
   count            = var.count_of_vpcs
-  name             = var.count_of_vpcs == "1" ? var.computer_route_name : "${var.computer_route_name}-${count.index + 1}"
+  name             = count.index < 1 ? var.computer_route_name : "${var.computer_route_name}-${count.index}"
   network          = google_compute_network.vpc_network[count.index].id
   dest_range       = var.dest_range
   next_hop_gateway = var.next_hop_gateway
