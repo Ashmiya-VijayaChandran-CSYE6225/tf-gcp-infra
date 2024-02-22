4c9698ad7b3ebd86bb8061cde0665222120bfa24
AshmiyaV <ashmiyavs6767@gmail.com>
2024-02-21 20:22:40 -0500

    [Assignment-4] variables

--- variables.tf
+++ variables.tf
@@ -13,11 +13,6 @@ variable "zone" {
   type        = string
 }
 
-variable "count_of_vpcs" {
-  description = "Count of VPCs to create"
-  type        = number
-}
-
 variable "vpc_network_name" {
   description = "Name of the VPC Network"
   type        = string
@@ -61,4 +56,89 @@ variable "dest_range" {
 variable "next_hop_gateway" {
   description = "The next hop gateway for the route to the computer"
   type        = string
+}
+
+variable "service_account_email" {
+  description = "Service Account Email"
+  type        = string
+}
+
+variable "google_compute_image" {
+  description = "Google Compute Image"
+  type        = string
+}
+
+variable "vm_instance_name" {
+  description = "VM Instance name"
+  type        = string
+}
+
+variable "vm_machine_type" {
+  description = "VM Machine Type"
+  type        = string
+}
+
+variable "vm_device_name" {
+  description = "VM Device Name"
+  type        = string
+}
+
+variable "initialize_params_type" {
+  description = "Initialize Params Type"
+  type        = string
+}
+
+variable "initialize_params_size" {
+  description = "Initialize Params Size"
+  type        = number
+}
+
+variable "google_compute_instance_tags" {
+  description = "Google Compute Instance Tags"
+  type        = list(string)
+}
+
+variable "service_account_scopes" {
+  description = "Service Account Scopes"
+  type        = list(string)
+}
+
+variable "google_compute_firewall_deny" {
+  description = "Firewall Deny"
+  type        = string
+}
+
+variable "google_compute_firewall_allow" {
+  description = "Firewall Allow"
+  type        = string
+}
+
+variable "google_compute_firewall_deny_description" {
+  description = "Firewall Deny Description"
+  type        = string
+}
+
+variable "google_compute_firewall_allow_description" {
+  description = "Firewall Allow Description"
+  type        = string
+}
+
+variable "source_tags" {
+  description = "Source Tags"
+  type = list(string)
+}
+
+variable "target_tags" {
+  description = "Target Tags"
+  type = list(string)
+}
+
+variable "source_ranges" {
+  description = "Source Ranges"
+  type = list(string)
+}
+
+variable "ports" {
+  description = "Ports"
+  type = list(string)
 }
